16b098589bc8b24a173e3102c479f460428aa03b
Wang Qin <wangqin0@outlook.com>
2025-04-04 11:39:03 -0700

    Add vcpkg support

---
+++ src/play_video/play_video.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+}
+
+
+auto main() -> int {
+  return 0;
+}
